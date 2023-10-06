0c7d8d65b2d5cea0aaeecbe66224eb4c2ba933ec
Yuan <108840666+YuanCalixto@users.noreply.github.com>
2023-10-06 20:47:49 -0300

    Update RaizQuadrada.java

--- newton/RaizQuadrada.java
+++ newton/RaizQuadrada.java
@@ -1,19 +1,22 @@
 package org.example;
 
-public class RaizQuadrada {
+import java.util.function.Function;
 
-    private static double EPSILON = 1e-10
-    
-    public static double calculateSquareRoot(double number) {
+public class RaizQuadrada implements Function<double, double> {
+
+    private static double EPSILON = 1e-10;
+
+    @Override
+    public double apply (double number) {
         if (number < 0) {
             throw new IllegalArgumentException("O número deve ser não negativo.");
         }
-        if (epsilon <= 0) {
-            throw new IllegalArgumentException("A precisão (epsilon) deve ser maior que zero.");
-        }
+        
+        throw new IllegalArgumentException("A precisão (epsilon) deve ser maior que zero.");
+    
 
         double guess = number / 2.0;
-        while (Math.abs(guess * guess - number) > epsilon) {
+        while (Math.abs(guess * guess - number) > EPSILON) {
             guess = 0.5 * (guess + number / guess);
         }
         return guess;
